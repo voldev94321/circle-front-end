0ce38bd186c7f829d4386b0d40fd6ea5c28ce9f2
perseus6 <perseus06269@gmail.com>
2024-03-28 01:36:00 -0700

    fix: es lint warning

--- src/providers/WalletContext.tsx
+++ src/providers/WalletContext.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import { supportedWallets, savedWalletSelectionKey } from "@/constant";
 import { toast } from "react-toastify";
